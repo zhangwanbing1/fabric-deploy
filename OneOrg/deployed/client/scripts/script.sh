e5851d3e7f5a598366a6e7141ad344c1b50bec48
zhangwanbing1 <zhangwanbing@live.com>
2019-05-27 15:41:14 +0800

    first commit

---
+++ OneOrg/deployed/client/scripts/script.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "Build your first network (BYFN) end-to-end test"
+echo
+CHANNEL_NAME="$1"
+DELAY="$2"
+LANGUAGE="$3"
+TIMEOUT="$4"
+VERBOSE="$5"
+NO_CHAINCODE="$6"
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${LANGUAGE:="golang"}
+: ${TIMEOUT:="10"}
+: ${VERBOSE:="false"}
+: ${NO_CHAINCODE:="false"}
+LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=10
+
+CC_SRC_PATH="github.com/chaincode/chaincode_example02/go/"
+if [ "$LANGUAGE" = "node" ]; then
+	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/node/"
+fi
+
+if [ "$LANGUAGE" = "java" ]; then
+	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/java/"
+fi
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import utils
+. scripts/utils.sh
+
+createChannel() {
+	setGlobals 0 1
+
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+                set -x
+		peer channel create -o orderer1.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+		res=$?
+                set +x
+	else
+				set -x
+		peer channel create -o orderer1.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+		res=$?
+				set +x
+	fi
+	cat log.txt
+	verifyResult $res "Channel creation failed"
+	echo "===================== Channel '$CHANNEL_NAME' created ===================== "
+	echo
+}
+
+joinChannel () {
+	#for org in 1 2; do
+	org=1
+	    for peer in 1 2; do
+		joinChannelWithRetry $peer $org
+		echo "===================== peer${peer}.org${org} joined channel '$CHANNEL_NAME' ===================== "
+		sleep $DELAY
+		echo
+	    done
+	#done
+}
+
+## Create channel
+echo "Creating channel..."
+#createChannel
+
+## Join all the peers to the channel
+echo "Having all peers join the channel..."
+joinChannel
+
+## Set the anchor peers for each org in the channel
+echo "Updating anchor peers for org1..."
+#updateAnchorPeers 1 1
+
+if [ "${NO_CHAINCODE}" != "true" ]; then
+
+	## Install chaincode on peer1.org1 and peer2.org1
+	echo "Installing chaincode on peer1.org1..."
+	installChaincode 1 1
+	echo "Install chaincode on peer2.org1..."
+	#installChaincode 2 1
+
+	# Instantiate chaincode on peer2.org1
+	echo "Instantiating chaincode on peer2.org1..."
+	#instantiateChaincode 2 1
+
+	# Query chaincode on peer0.org1
+	echo "Querying chaincode on peer2.org1..."
+	chaincodeQuery 1 1 100
+
+	# Invoke chaincode on peer1.org1 and peer2.org1
+	echo "Sending invoke transaction on peer1.org1 peer2.org1..."
+	chaincodeInvoke 1 1 
+	
+fi
+
+echo
+echo "========= All GOOD, BYFN execution completed =========== "
+echo
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+exit 0
