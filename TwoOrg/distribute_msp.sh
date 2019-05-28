78a7403ebedb4828c6d65ae25f8d53955e49d143
zhangwanbing1 <zhangwanbing@live.com>
2019-05-28 17:33:40 +0800

    调整TwoOrg目录和修改相关脚本

--- TwoOrg/distribute_msp.sh
+++ TwoOrg/distribute_msp.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-cp channel-artifacts/ crypto-config ../orderer1-ordererOrg/ -r
-scp -r channel-artifacts/ crypto-config xxx@172.16.2.60:/home/xxx/fabric-deploy
-scp -r channel-artifacts/ crypto-config xxx@192.168.14.95:/home/xxx/fabric-deploy/orderer3-ordererOrg
-scp -r channel-artifacts/ crypto-config xxx@192.168.14.95:/home/xxx/fabric-deploy/peer2-org1
-scp -r channel-artifacts/ crypto-config kylin@192.168.14.150:/home/kylin/fabric-deploy/peer1-org1
+# copy channel-artifacts and crypto-config to all node
+#orderer1
+cp channel-artifacts/ crypto-config deploy/ordererOrg-orderer1/ -r
+#orderer2
+cp channel-artifacts/ crypto-config deploy/ordererOrg-orderer2/ -r
+#orderer3
+cp channel-artifacts/ crypto-config deploy/ordererOrg-orderer3/ -r
+#org1-peer1
+cp channel-artifacts/ crypto-config deploy/org1-peer1/ -r
+#org1-peer2
+cp channel-artifacts/ crypto-config deploy/org1-peer2/ -r
+#org2-peer1
+cp channel-artifacts/ crypto-config deploy/org2-peer1/ -r
+#org2-peer2
+cp channel-artifacts/ crypto-config deploy/org2-peer2/ -r
+#client
+cp channel-artifacts/ crypto-config deploy/client/ -r
+
+# distribute the node to its running host
+scp -r deploy/ordererOrg-orderer1 xxx@172.16.2.59:/home/xxx/fabric-deploy/twoOrg
+scp -r deploy/ordererOrg-orderer2 xxx@172.16.2.60:/home/xxx/fabric-deploy/twoOrg
+scp -r deploy/ordererOrg-orderer3 xxx@172.16.2.61:/home/xxx/fabric-deploy/twoOrg
+
+scp -r deploy/org1-peer1 xxx@172.16.2.59:/home/xxx/fabric-deploy/twoOrg
+scp -r deploy/org1-peer2 xxx@172.16.2.60:/home/xxx/fabric-deploy/twoOrg
+scp -r deploy/org2-peer1 xxx@172.16.2.61:/home/xxx/fabric-deploy/twoOrg
+scp -r deploy/org2-peer2 xxx@172.16.2.95:/home/xxx/fabric-deploy/twoOrg
+
+cp -r deploy/client /home/xxx/fabric-deploy/twoOrg
